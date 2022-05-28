fc615382f5395c5bfa6dfa29b65531af1399362f
Josh Cotton <jcotton42@outlook.com>
2022-05-28 14:02:47 -0700

    More prompt commands.

--- src/Spear/Commands/PromptCommands.cs
+++ src/Spear/Commands/PromptCommands.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel;
+using Microsoft.EntityFrameworkCore;
 using Remora.Commands.Attributes;
 using Remora.Commands.Groups;
 using Remora.Discord.Commands.Conditions;
@@ -36,11 +37,77 @@ public class PromptCommands : CommandGroup {
         _spearContext.Prompts.Add(prompt);
         await _spearContext.SaveChangesAsync(CancellationToken);
 
-        var reply = await _feedback.SendContextualSuccessAsync(
-            $"That's an excellent suggestion! I'll add it to the list\n#`{prompt.Id}`\n\n>>> {suggestion}",
+        return await _feedback.SendContextualSuccessAsync(
+            $"That's an excellent suggestion! I'll add it to the list\n#`{prompt.Id}`\n>>> {suggestion}",
             ct: CancellationToken
         );
+    }
 
-        return reply.IsSuccess ? Result.FromSuccess() : Result.FromError(reply);
+    [Command("editprompt")]
+    [RequireContext(ChannelContext.Guild)]
+    [Description("Edits a prompt")]
+    public async Task<IResult> EditPromptAsync(
+        [Description("The prompt's ID")] int id,
+        [Description(@"The new prompt. Use `\n` for line breaks")] string newSuggestion
+    ) {
+        newSuggestion = newSuggestion.Replace("\\n", "\n");
+        var prompt = await _spearContext.Prompts
+            .FirstOrDefaultAsync(p => p.Id == id && p.GuildId == _commandContext.GuildID.Value, CancellationToken);
+        if(prompt is null) {
+            return Result.FromError(new NotFoundError($"No prompt found with ID {id}"));
+        }
+
+        // TODO check permissions (submitter can always edit, as well as anyone with Manage Guild)
+        prompt.Text = newSuggestion;
+        await _spearContext.SaveChangesAsync(CancellationToken);
+
+        return await _feedback.SendContextualSuccessAsync(
+            $"I have updated the suggestion.\n>>> {newSuggestion}",
+            ct: CancellationToken
+        );
+    }
+
+    [Command("deleteprompt")]
+    [RequireContext(ChannelContext.Guild)]
+    [Description("Deletes a prompt")]
+    public async Task<IResult> DeletePromptAsync(
+        [Description("The prompt's ID")] int id
+    ) {
+        var prompt = await _spearContext.Prompts
+            .FirstOrDefaultAsync(p => p.Id == id && p.GuildId == _commandContext.GuildID.Value, CancellationToken);
+        if(prompt is null) {
+            return Result.FromError(new NotFoundError($"No prompt found with ID {id}"));
+        }
+
+        // TODO check permissions
+        _spearContext.Prompts.Remove(prompt);
+        await _spearContext.SaveChangesAsync(CancellationToken);
+
+        return await _feedback.SendContextualSuccessAsync(
+            "I have deleted the prompt.",
+            ct: CancellationToken
+        );
+    }
+
+    [Command("prompt")]
+    [RequireContext(ChannelContext.Guild)]
+    [Description("Gets a random prompt")]
+    public async Task<IResult> PromptAsync() {
+        var ids = await _spearContext.Prompts
+            .Where(p => p.GuildId == _commandContext.GuildID.Value)
+            .Select(p => p.Id)
+            .ToListAsync(CancellationToken);
+        if(!ids.Any()) {
+            return Result.FromError(new NotFoundError("No prompts are available for this guild."));
+        }
+
+        var id = ids[Random.Shared.Next(ids.Count)];
+        var prompt = await _spearContext.Prompts.SingleAsync(p => p.Id == id, CancellationToken);
+        var creditLine = prompt.Submitter is not null ? $" by <@{prompt.Submitter}>" : "";
+
+        return await _feedback.SendContextualSuccessAsync(
+            $"May I suggest the following{creditLine}?\n#`{prompt.Id}`\n >>> {prompt.Text}",
+            ct: CancellationToken
+        );
     }
 }
