cba65abd5ed3886b7269295100583d13f5ecb011
Josh Cotton <jcotton42@outlook.com>
2023-02-05 18:03:15 -0800

    Redo devcontainer setup.

--- compose-rider.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-docker compose -p spear-devenv \
-    -f docker-compose.yml \
-    -f .devcontainer/docker-compose.devcontainer-common.yml \
-    -f .devcontainer/docker-compose.devcontainer-rider.yml \
-    "$@"
